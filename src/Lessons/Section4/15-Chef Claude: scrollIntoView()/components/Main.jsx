39113eafaac97e8d15f4aaf523a846888c078e2c
Yskrim <141317956+Yskrim@users.noreply.github.com>
2025-07-11 16:51:59 +1000

    15-Chef Claude: scrollIntoView()

---
+++ src/Lessons/Section4/15-Chef Claude: scrollIntoView()/components/Main.jsx
@@ -0,0 +1,71 @@
+import React from "react"
+import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientsList"
+
+export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState(
+        ["all the main spices", "pasta", "ground beef", "tomato paste"]
+    )
+
+
+    const [recipe, setRecipe] = React.useState("");
+    const recipeSection = React.useRef(null);
+
+    
+    React.useEffect(() => {
+        if(recipe && recipeSection.current){
+            try {
+                recipeSection.current.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+                inline: "nearest"})
+            } catch (error){
+                console.error("Scroll failed, ", error)
+            }
+        }
+    }, [recipe])
+    
+
+    async function getRecipe() {
+
+        console.log("loading")
+        const response = await fetch("http://localhost:3001/api/recipe", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ingredients: [...ingredients]}),
+        });
+        
+        const data = await response.json();
+        console.log("done!")
+        setRecipe(data.recipe);
+    }
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    return (
+        <main>
+
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            {ingredients.length > 0 && 
+                <IngredientsList 
+                    ref={recipeSection}
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                    />
+            }
+            {recipe ? <ClaudeRecipe recipe={recipe}/> : null} 
+        </main>
+    )
+}
