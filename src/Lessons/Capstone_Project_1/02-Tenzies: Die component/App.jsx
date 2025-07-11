7fd846976096f32f7cf80fce4bc5a2fc41920467
Yskrim <141317956+Yskrim@users.noreply.github.com>
2025-07-11 21:25:02 +1000

    02-Tenzies: Die component

---
+++ src/Lessons/Capstone_Project_1/02-Tenzies: Die component/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react-dom/client'
+import Die from './components/Die'
+
+export default function App(){
+
+/**
+     * Challenge:
+     * 
+     * - Create a Die component that takes a `value` prop
+     * - Render 10 instances of the Die component (manually)
+     *      - Provide a number between 1-6 for the value on each
+     *        for now
+     * - Style the <main> and <Die> components 
+     *   to look like they do in the slide
+     *      - Hints: Create a container to hold the 10 instances
+     *        of the Die component, and use CSS Grid to lay them
+     *        out evenly in 2 rows of 5 columns
+     *      - Use flexbox on main to center the dice container
+     *        in the center of the page
+     */
+    
+    // const [dice, setDice] = React.useState([])
+    // const diceElements = dice.map(d => {
+    //     <die value={d}/>
+    // })
+
+    return(
+        <main>
+            <div className="container">
+                <Die value='2'/>
+                <Die value='2'/>
+                <Die value='1'/>
+                <Die value='2'/>
+                <Die value='4'/>
+
+                <Die value='6'/>
+                <Die value='4'/>
+                <Die value='3'/>
+                <Die value='1'/>
+                <Die value='5'/>
+            </div>
+            
+            
+        </main>
+    )
+}
